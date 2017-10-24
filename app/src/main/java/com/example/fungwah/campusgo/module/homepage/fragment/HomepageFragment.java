cebd4923fccf2a068ca41c18f866676030119086
ofw <906372411@qq.com>
2017-10-24 13:21:15 +0800

    完善一部分主页

--- app/src/main/java/com/example/fungwah/campusgo/module/homepage/fragment/HomepageFragment.java
+++ app/src/main/java/com/example/fungwah/campusgo/module/homepage/fragment/HomepageFragment.java
@@ -1,16 +1,14 @@
 package com.example.fungwah.campusgo.module.homepage.fragment;
 
-import android.app.Dialog;
+import android.animation.ObjectAnimator;
 import android.graphics.Color;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Display;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.fungwah.campusgo.R;
 import com.example.fungwah.campusgo.command.adapter.FragmentPageAdapter;
@@ -32,8 +30,28 @@ public class HomepageFragment extends BaseFragment implements View.OnClickListen
     private FragmentPagerAdapter pagerAdapter;
     private List<BaseFragment> fragmentList = new ArrayList<>();
 
-    private Dialog dialog;
-    private View dialogView;
+    private RelativeLayout coverRl;
+    private FloatingActionButton activityAddFAB;
+    private FloatingActionButton courseAddFAB;
+    private TextView courseTv;
+    private TextView activityTv;
+
+    private ObjectAnimator activityFabTranslationAppearAnim;
+    private ObjectAnimator courseFabTranslationAppearAnim;
+    private ObjectAnimator activityFabTransilationDisappearAnim;
+    private ObjectAnimator courseFabTranslationDisappearAnim;
+
+    private ObjectAnimator activityFabAlphaAppearAnim;
+    private ObjectAnimator activityFabAlphaDisappearAnim;
+    private ObjectAnimator courseFabAlphaAppearAnim;
+    private ObjectAnimator courseFabAlphaDisappearAnim;
+
+    private ObjectAnimator addFabRotateRight;
+    private ObjectAnimator addFabRotateLeft;
+
+
+//    private Dialog dialog;
+//    private View dialogView;
 
     @Override
     protected void initObject() {
@@ -48,14 +66,20 @@ public class HomepageFragment extends BaseFragment implements View.OnClickListen
     protected void initView(View parent) {
         initFragmentList();
 
+        coverRl = findView(R.id.homepage_cover_rl);
+        activityAddFAB = findView(R.id.add_activity_fab);
+        courseAddFAB = findView(R.id.add_course_fab);
+        activityTv = findView(R.id.homepage_activity_fab_tv);
+        courseTv = findView(R.id.homepage_course_fab_tv);
         addFAB = findView(R.id.homepage_add_fab);
         tabLayout = findView(R.id.homepage_tab_layout);
         viewPager = findView(R.id.homepage_fragment_content_vp);
         // TODO: 2017/10/23  此处注意，Fragment 嵌套使用时，子 fragment 添加的是 ChildFragmentManager 而非 SupportFragmentManager ,回头了解一下
         pagerAdapter = new FragmentPageAdapter(fragmentList, getChildFragmentManager());
 
-        dialog = new Dialog(getContext(), R.style.MyDialog);
-        dialogView = LayoutInflater.from(getContext()).inflate(R.layout.include_add_fab_layout, null);
+//        dialog = new Dialog(getContext(), R.style.MyDialog);
+//        dialogView = LayoutInflater.from(getContext()).inflate(R.layout.include_add_fab_layout, null);
+        initAnimator();
     }
 
     private void initFragmentList() {
@@ -66,14 +90,16 @@ public class HomepageFragment extends BaseFragment implements View.OnClickListen
 
     @Override
     protected void setView() {
-        dialogView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        dialog.setContentView(dialogView);
+        coverRl.setAlpha(0);
+
+//        dialogView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+//        dialog.setContentView(dialogView);
 
         //全屏显示Dialog
-        WindowManager windowManager = dialog.getWindow().getWindowManager();
-        Display display = windowManager.getDefaultDisplay();
-        WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
-        params.width = display.getWidth();
+//        WindowManager windowManager = dialog.getWindow().getWindowManager();
+//        Display display = windowManager.getDefaultDisplay();
+//        WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
+//        params.width = display.getWidth();
 
         viewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
@@ -94,10 +120,43 @@ public class HomepageFragment extends BaseFragment implements View.OnClickListen
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.homepage_add_fab:
-                dialog.show();
+//                dialog.show();
+
                 break;
             default:
                 break;
         }
     }
+
+    protected void initAnimator() {
+        activityFabTranslationAppearAnim = ObjectAnimator.ofFloat(activityAddFAB, "translationY", 60, 0).setDuration(200);
+        activityFabTransilationDisappearAnim = ObjectAnimator.ofFloat(activityAddFAB, "translationY", 0, 60).setDuration(200);
+        courseFabTranslationAppearAnim = ObjectAnimator.ofFloat(courseAddFAB, "translationY", 30, 0).setDuration(200);
+        courseFabTranslationDisappearAnim = ObjectAnimator.ofFloat(courseAddFAB, "translationY", 0, 30).setDuration(200);
+        addFabRotateLeft = ObjectAnimator.ofFloat(addFAB, "rotation", 0, 90).setDuration(200);
+        addFabRotateLeft = ObjectAnimator.ofFloat(addFAB, "rotation", 90, 0).setDuration(200);
+        activityFabAlphaAppearAnim = ObjectAnimator.ofFloat(activityAddFAB, "alpha", 0, 1).setDuration(200);
+        activityFabAlphaDisappearAnim = ObjectAnimator.ofFloat(activityAddFAB, "alpha", 1, 0).setDuration(200);
+        courseFabAlphaAppearAnim = ObjectAnimator.ofFloat(activityAddFAB, "alpha", 0, 1).setDuration(200);
+        courseFabAlphaDisappearAnim = ObjectAnimator.ofFloat(activityAddFAB, "alpha", 1, 0).setDuration(200);
+    }
+
+    protected void appearAnim() {
+
+    }
+
+    protected void visiableFab() {
+        activityAddFAB.setVisibility(View.VISIBLE);
+        courseAddFAB.setVisibility(View.VISIBLE);
+        activityTv.setVisibility(View.VISIBLE);
+        courseTv.setVisibility(View.VISIBLE);
+    }
+
+    protected void inVisiableFab() {
+        activityAddFAB.setVisibility(View.INVISIBLE);
+        courseAddFAB.setVisibility(View.INVISIBLE);
+        activityTv.setVisibility(View.INVISIBLE);
+        courseTv.setVisibility(View.INVISIBLE);
+    }
+
 }
